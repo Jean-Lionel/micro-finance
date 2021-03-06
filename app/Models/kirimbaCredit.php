8b6b5e165a4d4d0b2565b201ded3dde68162024a
Jean Lionel <nijeanlionel@gmail.com>
2021-03-06 09:12:26 +0200

    Add calc interet on ikirimba

---
+++ app/Models/kirimbaCredit.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Auth;
+
+class kirimbaCredit extends Model
+{
+    //
+    use SoftDeletes;
+
+    protected $guarded = [];
+
+    public static function boot(){
+    	parent::boot();
+
+    	self::creating(function($model){
+    		$model->user_id = Auth::user()->id;
+    	});
+    }
+}
