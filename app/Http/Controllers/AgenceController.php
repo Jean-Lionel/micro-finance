20a4232bbe5394433bf867295ab109ac9cee9d3e
Jean Lionel <nijeanlionel@gmail.com>
2021-04-14 04:46:20 +0200

    create agence controller

---
+++ app/Http/Controllers/AgenceController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AgenceController extends Controller
+{
+    //
+
+    public function index() {
+    	return view("agences.index");
+    }
+}
