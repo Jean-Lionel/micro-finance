dae20ec5ba56c4433afc51ca597eeae5745e8054
Jean Lionel <nijeanlionel@gmail.com>
2021-04-19 16:55:43 +0200

    caisse caissier

---
+++ database/migrations/2021_04_19_165305_create_caisse_caissiers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCaisseCaissiersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('caisse_caissiers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->double('montant',64,2);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('caisse_caissiers');
+    }
+}
