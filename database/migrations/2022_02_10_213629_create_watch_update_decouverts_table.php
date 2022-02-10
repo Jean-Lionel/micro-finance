ce5b4bf325f4e5c44f3d41ede1e62d59a4c0e8e0
Jean-Lionel <nijeanlionel@gmail.com>
2022-02-10 21:42:42 +0200

    add update decouvert watch

---
+++ database/migrations/2022_02_10_213629_create_watch_update_decouverts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWatchUpdateDecouvertsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('watch_update_decouverts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('decouvert_id');
+            $table->integer('periode');
+            $table->text('description');
+            $table->foreignId('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('watch_update_decouverts');
+    }
+}
